26fc200d6ca14b418f7612488303f5de5e085057
anoy <545544032@qq.com>
2019-03-28 13:54:59 +0800

    Update

---
+++ 00 - 注册登录/README.md
@@ -0,0 +1,10 @@
+## 使用手册
+
+1、执行 `mvn spring-boot:run`
+
+2、访问 http://localhost:8080/
+
+3、点击 `登录`
+
+> 账号：anoyi
+> 密码：anoyi
---
+++ 00 - 注册登录/src/main/java/com/spring4all/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.spring4all.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    /**
+     * 匹配 "/" 路径，不需要权限即可访问
+     * 匹配 "/user" 及其以下所有路径，都需要 "USER" 权限
+     * 登录地址为 "/login"，登录成功默认跳转到页面 "/user"
+     * 退出登录的地址为 "/logout"，退出成功后跳转到页面 "/login"
+     * 默认启用 CSRF
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/user")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+    }
+
+    /**
+     * 在内存中创建一个名为 "user" 的用户，密码为 "pwd"，拥有 "USER" 权限
+     */
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("anoyi").password("anoyi").roles("USER").build());
+        return manager;
+    }
+
+}
